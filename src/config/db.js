6251ee262a7a1e7daa7d408c94da9ebe539e834e
Sebastian <sebas.music06@gmail.com>
2026-03-03 23:24:21 -0500

    Actualizacion base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
 });
 
 module.exports = pool;
